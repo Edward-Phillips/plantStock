38293a270672d3f406d76c181233a86091d5f861
Edward Phillips <edd0ddi0@gmail.com>
2020-09-14 16:13:20 +0100

    add delete product method

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,3 +1,4 @@
+import { restart } from 'nodemon';
 import Model from '../models/model';
 
 const productsModel = new Model('products');
@@ -48,3 +49,21 @@ export const updateProduct = async (req, res) => {
     res.status(200).json({ products: err.stack });
   }
 };
+
+export const deleteProduct = async (req, res) => {
+  const {
+    product_name,
+    cutting_type 
+  } = req.body;
+  const columns = [ 'product_name', 'cutting_type' ];
+  const values = [`'${product_name}'`, `'${cutting_type}'`]
+  try {
+    const data = await productsModel.deleteWithReturn(
+      columns,
+      values
+    );
+    res.status(200).json({ products: data.rows });
+  } catch (err) {
+    res.status(200).json({ products: err.stack });
+  }
+}
