72d31e38d95451abc1d5731176251acd98cf2731
Edward Phillips <edd0ddi0@gmail.com>
2020-09-14 15:42:24 +0100

    Add route for updating a product

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { indexPage, stockPage, productsPage, addProduct } from '../controllers';
+import { indexPage, stockPage, productsPage, addProduct, updateProduct } from '../controllers';
 
 const indexRouter = express.Router();
 
@@ -7,5 +7,6 @@ indexRouter.get('/', indexPage);
 indexRouter.get('/stock', stockPage);
 indexRouter.get('/products', productsPage);
 indexRouter.post('/products', addProduct);
+indexRouter.put('/products', updateProduct);
 
 export default indexRouter;
