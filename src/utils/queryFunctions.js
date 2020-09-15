17d3e54eaa16c74cd63fda095ad478e1f7fa4b31
Edward Phillips <edd0ddi0@gmail.com>
2020-09-15 10:56:46 +0100

    add query array to ensure no asynchronous issues.

--- src/utils/queryFunctions.js
+++ src/utils/queryFunctions.js
@@ -18,3 +18,4 @@ export const executeQueryArray = async (arr) => new Promise((resolve) => {
 export const dropTables = () => executeQueryArray([ dropProductsTable ]);
 export const createTables = () => executeQueryArray([ createProductsTable, createStockTable ]);
 export const insertIntoTables = () => executeQueryArray([ insertProducts, insertStock ]);
+export const createAndInsertIntoTables = () => executeQueryArray([ createProductsTable, createStockTable, insertProducts, insertStock ]);
