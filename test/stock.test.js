e540d9271995cb9bd9f387a1e9e06a80d07e7f66
Edward Phillips <edd0ddi0@gmail.com>
2020-09-16 15:33:18 +0100

    it posts stock

--- test/stock.test.js
+++ test/stock.test.js
@@ -17,4 +17,29 @@ describe('stock resource', () => {
         done();
       });
   });
+
+  it('posts stock', (done) => {
+    const data = {
+      product_name: 'Philodendron Squamiferum',
+      cutting_type: 'rooted',
+      cost_per_cutting: '1.5',
+      current_count: '0.34',
+    };
+    server
+      .post(`${BASE_URL}/stock`)
+      .send(data)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.stock).to.be.instanceOf(Array);
+        res.body.stock.forEach((m) => {
+          expect(m).to.have.property('id');
+          expect(m).to.have.property('product_id', data.product_name);
+          expect(m).to.have.property('cost_per_cutting', data.cost_per_cutting);
+          expect(m).to.have.property('cutting_type', data.cutting_type);
+          expect(m).to.have.property('current)count', data.current_count);
+        });
+        done();
+      });
+  });
 });
