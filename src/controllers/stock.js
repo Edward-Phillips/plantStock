7197bb644bbab472a129bc3fb1032593268eb3ac
Edward Phillips <edd0ddi0@gmail.com>
2020-09-16 22:38:49 +0100

    add method to add stock to stock database

--- src/controllers/stock.js
+++ src/controllers/stock.js
@@ -12,3 +12,21 @@ export const stockPage = async (req, res) => {
     res.status(200).json({ stock: err.stack });
   }
 };
+
+export const addStock = async (req, res) => {
+  const {
+    product_name,
+    cost_per_cutting,
+    current_count,
+  } = req.body;
+  const { product_id } = req;
+  const columns = 'product_id, cost_per_cutting, current_count ';
+  const values = `${product_id}, ${cost_per_cutting}, ${current_count}`;
+  try {
+    const data = await stockModel.insertWithReturn(columns, values);
+    data.rows[0].product_name = product_name;
+    res.status(200).json({ stock: data.rows });
+  } catch (err) {
+    res.status(200).json({ stock: err.stack });
+  }
+};
