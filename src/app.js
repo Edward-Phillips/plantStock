97aee0bc60fd249afa6b57b86efc90b9ed493962
Edward Phillips <edd0ddi0@gmail.com>
2020-09-18 16:19:58 +0100

    remove cors headers, update corsOptions variable

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import indexRouter from './routes/index';
 
 const cors = require('cors');
 const corsOptions = {
-  origin: 'plantstock.surge.sh',
+  origin: 'http://plantstock.surge.sh',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
@@ -16,12 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use('/v1', indexRouter);
-app.use((req,res,next) => {
-  res.header("Access-Conrol-Allow-Origin",'*');
-  res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 app.use((err, req, res, next) => {
   res.status(400).json({ error: err.stack });
 });
