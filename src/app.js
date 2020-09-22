b988d05d853463aa24cfdaff3d05180b27aa9963
Edward Phillips <edd0ddi0@gmail.com>
2020-09-22 12:25:55 +0100

    update CORS to allow https/http for plantstock.surge.sh

--- src/app.js
+++ src/app.js
@@ -4,8 +4,16 @@ import cookieParser from 'cookie-parser';
 import indexRouter from './routes/index';
 
 const cors = require('cors');
+
+const whitelist = ['http://plantstock.surge.sh', 'https://plantstock.surge.sh'];
 const corsOptions = {
-  origin: 'http://plantstock.surge.sh',
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
