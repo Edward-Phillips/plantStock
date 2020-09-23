b4ea423ed412f36c9c921f29fb76a528990ab9b8
Edward Phillips <edd0ddi0@gmail.com>
2020-09-23 16:04:42 +0100

    add updateCustomer method for updating name/address of customer

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -24,3 +24,28 @@ export const addCustomer = async (req, res) => {
     res.status(200).json({ customers: err.stack });
   }
 };
+
+export const updateCustomer = async (req, res) => {
+  const {
+    old_name,
+    old_address,
+    name,
+    address
+  } = req.body;
+  const columns = [ 'name', 'address' ];
+  const constraintValues = [ `'${old_name}'`, `'${old_address}'` ];
+  const values = [ `'${name}'`, `'${address}'` ];
+  console.log(constraintValues);
+  console.log(values);
+  try {
+    const data = await customersModel.updateWithReturn(
+      columns,
+      constraintValues,
+      columns,
+      values
+    );
+    res.status(200).json({ customers: data.rows });
+  } catch (err) {
+    res.status(200).json({ customers: err.stack });
+  }
+};
