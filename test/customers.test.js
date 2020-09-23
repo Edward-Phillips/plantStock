8c9517f13618aa3fe3610f831d58d4535e6a2570
Edward Phillips <edd0ddi0@gmail.com>
2020-09-23 13:10:49 +0100

    test for making post request to /customers

--- test/customers.test.js
+++ test/customers.test.js
@@ -15,4 +15,24 @@ describe('Customers', () => {
         done();
       });
   });
+
+  it('posts customers', (done) => {
+    const data = {
+      name: 'plant name',
+      address: 'Number 49, Seventh Square'
+    };
+    server
+      .post(`${BASE_URL}/customers`)
+      .send(data)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.customers).to.be.instanceOf(Array);
+        res.body.customers.forEach((m) => {
+          expect(m).to.have.property('name', data.name);
+          expect(m).to.have.property('address', data.address);
+        });
+        done();
+      });
+  });
 });
