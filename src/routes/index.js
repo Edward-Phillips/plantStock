d7ce9118daa04b331c346511576f3920932239ed
Edward Phillips <edd0ddi0@gmail.com>
2020-09-23 11:42:56 +0100

    separate different resource routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,17 +8,23 @@ import {
   addProduct,
   updateProduct,
   deleteProduct,
+  customersPage,
 } from '../controllers';
 import { productIdFinder } from '../middleware';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
+
 indexRouter.get('/stock', stockPage);
 indexRouter.post('/stock', productIdFinder, addStock);
 indexRouter.put('/stock', productIdFinder, updateStock);
+
 indexRouter.get('/products', productsPage);
 indexRouter.post('/products', addProduct);
 indexRouter.put('/products', updateProduct);
 indexRouter.delete('/products', deleteProduct);
+
+indexRouter.get('/customers', customersPage);
+
 export default indexRouter;
