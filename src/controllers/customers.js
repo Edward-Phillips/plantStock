ea7f990d91b0baaab9f09308631bab00cf9c3e4f
Edward Phillips <edd0ddi0@gmail.com>
2020-09-23 13:11:03 +0100

    add addCustomer controller for adding a customer

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -12,3 +12,15 @@ export const customersPage = async (req, res) => {
     res.status(200).json({ customers: err.stack });
   }
 };
+
+export const addCustomer = async (req, res) => {
+  const { name, address } = req.body;
+  const columns = 'name, address';
+  const values = `'${name}', '${address}'`;
+  try {
+    const data = await customersModel.insertWithReturn(columns, values);
+    res.status(200).json({ customers: data.rows });
+  } catch (err) {
+    res.status(200).json({ customers: err.stack });
+  }
+};
