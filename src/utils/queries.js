3f019cefea4943557c8aef0b6216dc1fd657943b
Edward Phillips <edd0ddi0@gmail.com>
2020-10-06 22:50:05 +0100

    add orders & order_items tables, example values for orders

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -61,6 +61,20 @@ CREATE TABLE IF NOT EXISTS customers(
   name VARCHAR ( 255 ) NOT NULL,
   address VARCHAR ( 255 ) NOT NULL
 );
+CREATE TABLE IF NOT EXISTS orders(
+  id serial PRIMARY KEY,
+  customer_id INTEGER REFERENCES customers(id),
+  order_date VARCHAR( 255 ) NOT NULL,
+  order_total NUMERIC NOT NULL
+);
+CREATE TABLE IF NOT EXISTS order_items(
+  id serial PRIMARY KEY,
+  order_id INTEGER REFERENCES orders(id),
+  stock_id INTEGER REFERENCES stock(id),
+  count INTEGER NOT NULL,
+  price NUMERIC,
+  cost_per_item NUMERIC
+);
 INSERT INTO products(product_name, price, cutting_type)
 VALUES
   ('Philodendron Squamiferum', 3, 'rooted'),
@@ -74,4 +88,8 @@ INSERT INTO customers(name, address)
 VALUES
   ('Steve', '45 the house on the hill'),
   ('Gerald', '14 The Shieldwall, Arrakis');
+INSERT INTO orders(customer_id, order_date, order_total )
+VALUES
+  (1, 'timestamp here', 90.00),
+  (2, '19th September 2020', 14.5);
 `;
