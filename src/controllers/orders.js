f0e2665e7323c52e1f783bb339fee60626accbc3
Edward Phillips <edd0ddi0@gmail.com>
2020-10-20 19:19:11 +0100

    add addOrder function

--- src/controllers/orders.js
+++ src/controllers/orders.js
@@ -1,6 +1,9 @@
 import Model from '../models/model';
+import ResourceHandler from './resourceHandler';
 
 const ordersModel = new Model('orders');
+
+const ordersHandler = new ResourceHandler('orders');
 
 export const getOrdersForCustomer = async (req, res) => {
   const { customerId } = req.params;
@@ -11,4 +14,12 @@ export const getOrdersForCustomer = async (req, res) => {
   } catch (err) {
     res.status(200).json({ orders: err.stack });
   }
+};
+
+export const addOrder = async(req, res) => {
+  const {customer_id, order_date, order_total} = req.body;
+  const columns = 'customer_id, order_date, order_total';
+  const values = `'${customer_id}', '${order_date}', '${order_total}'`;
+  const queryResult = await ordersHandler.addResource(columns, values);
+  res.status(200).json({order: queryResult});
 };
