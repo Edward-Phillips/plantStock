0accbeb1a40d04597e8fb4148432366ca0d2590e
Edward Phillips <edd0ddi0@gmail.com>
2020-10-20 19:17:56 +0100

    can post an order for a particular customer

--- test/orders.test.js
+++ test/orders.test.js
@@ -16,4 +16,37 @@ describe('Orders', () => {
         done();
       });
   });
+
+  it('can post a new order for a particular customer', (done) => {
+    const data = {
+      customer_id: 1,
+      order_date: 'timestamp here',
+      order_total: 90.00,
+      order_items: [{
+        stock_id: 1,
+        count: 30,
+        price: 3,
+        cost_per_cutting: 0.35
+      }]
+    };
+    server
+      .post(`${BASE_URL}/orders`)
+      .send(data)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.order[0]).to.have.property('order_date', 'timestamp here');
+        expect(res.body.order[0]).to.have.property('order_total', '90');
+        expect(res.body.order[0]).to.have.property('customer_id', 1);
+        expect(res.body.order[0]).to.have.property('id');
+        res.body.order_items.forEach((orderItem) => {
+          expect(orderItem).to.have.property('stock_id', 1);
+          expect(orderItem).to.have.property('count', 30);
+          expect(orderItem).to.have.property('price', 3);
+          expect(orderItem).to.have.property('cost_per_cutting', 0.35);
+          expect(orderItem).to.have.property('order_id', res.body.order[0].id);
+        });
+        done();
+      });
+  });
 });
