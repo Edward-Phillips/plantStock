45740dc03cfe407a20d6baa08b158f3f8efcb588
Edward Phillips <edd0ddi0@gmail.com>
2020-10-21 14:17:33 +0100

    convert addOrder function to middleware, add addOrderItems to return orderedItems & order object in Json

--- src/controllers/orders.js
+++ src/controllers/orders.js
@@ -5,6 +5,8 @@ const ordersModel = new Model('orders');
 
 const ordersHandler = new ResourceHandler('orders');
 
+const orderItemsHandler = new ResourceHandler('order_items')
+
 export const getOrdersForCustomer = async (req, res) => {
   const { customerId } = req.params;
   try {
@@ -16,10 +18,28 @@ export const getOrdersForCustomer = async (req, res) => {
   }
 };
 
-export const addOrder = async(req, res) => {
-  const {customer_id, order_date, order_total} = req.body;
+export const addOrder = async(req, res, next) => {
+  const { customer_id, order_date, order_total } = req.body;
   const columns = 'customer_id, order_date, order_total';
   const values = `'${customer_id}', '${order_date}', '${order_total}'`;
   const queryResult = await ordersHandler.addResource(columns, values);
-  res.status(200).json({order: queryResult});
+  const { id } = queryResult[0];
+  req.order_id = id;
+  req.order = queryResult;
+  next();
+};
+
+
+export const addOrderItems = async(req, res, next) => {
+  const { order_items } = req.body;
+  const { order_id } = req;
+  const columns = 'order_id, stock_id, count, price, cost_per_item';
+  const orderedItems = [];
+  order_items.map(async (item) => {
+    const { stock_id, count, price, cost_per_cutting } = item;
+    const values = `'${order_id}', '${stock_id}', '${count}', '${price}', '${cost_per_cutting}'`;
+    const queryResult = await orderItemsHandler.addResource(columns, values);
+    orderedItems.push(queryResult[0]);
+  });
+  res.status(200).json({order: req.order, order_items: orderedItems});
 };
